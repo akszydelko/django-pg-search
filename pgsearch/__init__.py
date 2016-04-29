5ff70d0bac316c5fc94f55a51ea06ee28ed7a251
Arkadiusz Szydełko <arekxd@gmail.com>
2016-04-29 11:04:26 +0200

    Allow headlines with simple setting or with choosen language, hedline is not unaccented.

--- pgsearch/__init__.py
+++ pgsearch/__init__.py
@@ -3,3 +3,26 @@ Minimalistic application with fields, managers etc. for full text search support
 
 Inspired by: https://github.com/linuxlewis/djorm-ext-pgfulltext
 """
+
+FTS_CONFIGURATIONS = {
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'fi': 'finnish',
+    'fr': 'french',
+    'de': 'german',
+    'hu': 'hungarian',
+    'it': 'italian',
+    'no': 'norwegian',
+    'nb': 'norwegian',
+    'nn': 'norwegian',
+    'pt': 'portuguese',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'simple': 'simple',
+    'es': 'spanish',
+    'sv': 'swedish',
+    'tr': 'turkish',
+}
+
+FTS_CONFIGURATIONS.update({v: v for v in FTS_CONFIGURATIONS.values()})
--- pgsearch/query.py
+++ pgsearch/query.py
@@ -2,6 +2,8 @@ import re
 from django.db.models import QuerySet
 from psycopg2._psycopg import adapt
 
+from . import FTS_CONFIGURATIONS
+
 
 class BaseQuerySet(QuerySet):
     @staticmethod
@@ -24,17 +26,21 @@ class SearchQuerySet(BaseQuerySet):
     def __startswith(self, word_list):
         return [x.strip() + ":*" for x in filter(lambda x: x.strip(), word_list)]
 
-    def __get_tsquery(self, term, config, join_by, startswith):
+    def __unaccent(self, term):
+        return 'unaccent(%s)' % term
+
+    def __get_tsquery(self, term, config, join_by, startswith, unaccent=True):
         operator = " %s " % join_by
         query = self.__startswith(term) if startswith else term
-        return ("to_tsquery('%s', unaccent(%s))" % (
+        query = adapt((operator.join(query)).encode('utf-8'))
+        return ("to_tsquery('%s', %s)" % (
             config,
-            adapt((operator.join(query)).encode('utf-8'))
+            self.__unaccent(query) if unaccent else query
         )).decode('utf-8')
 
     def search(self, search_term, using=None, rank_ordering=True, rank_function='ts_rank', rank_normalization=32,
                fts_language="english", include_simple=True, join_by="&", startswith=True, divide_by_title_length=False,
-               headline_field=None):
+               headline_field=None, headline_lang_simple=False):
         """Use full text search utilities for looking through all objects."""
         qs = self
 
@@ -45,9 +51,10 @@ class SearchQuerySet(BaseQuerySet):
             if isinstance(search_term, basestring):
                 search_term = [x for x in re.sub('[\W-]+', ' ', search_term, flags=re.UNICODE).split(" ") if x]
 
+            fts_language = FTS_CONFIGURATIONS.get(fts_language, 'simple')
             ts_query = self.__get_tsquery(search_term, fts_language, join_by, startswith)
 
-            if include_simple:
+            if include_simple and fts_language != 'simple':
                 ts_query += ' || ' + self.__get_tsquery(search_term, 'simple', join_by, startswith)
 
             ts_query = '(%s)' % ts_query
@@ -79,10 +86,11 @@ class SearchQuerySet(BaseQuerySet):
                 select_dict['rank'] = '(%s)' % select_dict['rank']
 
             if headline_field:
+                headline_lang = "simple" if headline_lang_simple else fts_language
                 select_dict[headline_field] = "ts_headline('%s', %s, %s)" % (
-                    fts_language,
+                    headline_lang,
                     "%s.%s" % (self.qn(self.model._meta.db_table), self.qn(headline_field)),
-                    ts_query
+                    self.__get_tsquery(search_term, headline_lang, join_by, startswith, unaccent=False)
                 )
 
             qs = qs.extra(select=select_dict, where=[where], order_by=[order])
